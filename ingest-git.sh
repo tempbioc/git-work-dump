185fca454443c0c8dac10e81179a7d5f4fd3b648
Alexandru Mahmoud <almahmoudresearch@gmail.com>
2024-08-20 15:03:01 -0400

    Additions

--- ingest-git.sh
+++ ingest-git.sh
@@ -10,22 +10,25 @@ if ! gh auth status &> /dev/null; then
     exit 1
 fi
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <file_with_git_urls>"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <organization-name> <file_with_git_urls>"
     exit 1
 fi
 
-URL_FILE=$1
-ORG_NAME="tempbioc"
+ORG_NAME="$1"
+URL_FILE="$2"
+pkginfo="/tmp/pkginfo"
 
 while IFS= read -r repo_url; do
     REPO_NAME=$(basename "$repo_url")
     
     echo "Processing repository: $REPO_NAME"
+    git ls-remote https://github.com/$ORG_NAME/$REPO_NAME devel > $pkginfo && if [ -z "$(cat $pkginfo)" ]; then echo "$REPO_NAME exists and is empty"; else echo "$REPO_NAME exists and is not empty."; continue; fi || ( gh repo create "$ORG_NAME/$REPO_NAME" --public && sleep 10 || sleep 6000 )
+    rm -rf $pkginfo
 
     echo "Cloning $repo_url into $ORG_NAME/$REPO_NAME"
-    git clone "$repo_url" "$REPO_NAME" && gh repo create "$ORG_NAME/$REPO_NAME" --public || exit
-    cd "$REPO_NAME"
+    git clone "$repo_url" "$REPO_NAME" || cd "$REPO_NAME"/.. || echo "$repo_url" >> exceptions
+    cd "$REPO_NAME" || continue
     git remote remove origin
     git remote add origin "https://github.com/$ORG_NAME/$REPO_NAME.git"
     git push -u origin --all
