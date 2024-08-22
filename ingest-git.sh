6caa65150ae95021b99069d695b72866a1fc1870
Alexandru Mahmoud <almahmoudresearch@gmail.com>
2024-08-22 15:48:22 -0400

    Add large file list

--- ingest-git.sh
+++ ingest-git.sh
@@ -24,17 +24,18 @@ while IFS= read -r repo_url; do
     
     echo "Processing repository: $REPO_NAME"
     git ls-remote https://github.com/$ORG_NAME/$REPO_NAME devel > $pkginfo && if [ -z "$(cat $pkginfo)" ]; then echo "$REPO_NAME exists and is empty"; else echo "$REPO_NAME exists and is not empty."; continue; fi || ( gh repo create "$ORG_NAME/$REPO_NAME" --public && sleep 10 || sleep 6000 )
-    rm -rf $pkginfo
+    rm -f $pkginfo
 
     echo "Cloning $repo_url into $ORG_NAME/$REPO_NAME"
-    git clone "$repo_url" "$REPO_NAME" || cd "$REPO_NAME"/.. || echo "$repo_url" >> exceptions
+    mkdir -p "logs/$REPO_NAME"
+    ( git clone "$repo_url" "$REPO_NAME" || cd "$REPO_NAME"/.. || echo "$repo_url" >> exceptions ) 2>&1 | tee logs/$REPO_NAME/clone_log
     cd "$REPO_NAME" || continue
     git remote remove origin
     git remote add origin "https://github.com/$ORG_NAME/$REPO_NAME.git"
-    git push -u origin --all
+    git push -u origin --all 2>&1 | tee ../logs/$REPO_NAME/push_log 
 
     cd ..
-    rm -rf "$REPO_NAME"
+    #rm -rf "$REPO_NAME"
 
     echo "Finished processing $REPO_NAME"
 done < "$URL_FILE"
